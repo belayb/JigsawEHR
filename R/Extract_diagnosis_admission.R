896ffc1ad43380e5faaa041a44d7694185e026fa
belayb <belaybirlie.yimer@manchester.ac.uk>
2022-03-28 15:33:05 +0100

    extract diagnosis

---
+++ R/Extract_diagnosis_admission.R
@@ -0,0 +1,12 @@
+#' Make a diagnosis table at admission
+#'
+#' @param data a spark data frame
+#' @param diagnosis a character (list of character vector) specifying the condition(s) of interest
+#'
+#' @return
+#' @export
+#'
+#' @examples
+Extract_diagnosis_admission <- function(data, diagnosis){
+
+}
---
+++ R/Extract_procedure_admission.R
@@ -0,0 +1,12 @@
+#' Make a procedure table at admission
+#'
+#' @param data a spark data frame
+#' @param diagnosis a character (list of character vector) specifying the type of procedure(s) of interest
+#'
+#' @return
+#' @export
+#'
+#' @examples
+Extract_procedure_admission <- function(data, procedure){
+
+}
