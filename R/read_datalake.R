7c209681502ed0381679f12a9105f8efde47789f
Belay Birlie Yimer <34164105+belayb@users.noreply.github.com>
2023-01-21 21:04:16 +0000

    reading csv file

--- R/read_datalake.R
+++ R/read_datalake.R
@@ -1,8 +1,9 @@
 
 #' Read a tabular data file into a Spark DataFram from datalake storage account
 #'
-#' @param filename The name of the data file (with its extension) to be imported
+#' @parm dir - path to the dbfs directory or container 
 #' @param folder The name of the folder with in the storage account that holds 'filename'
+#' @param filename The name of the data file (with its extension) to be imported
 #'
 #' @importFrom sparklyr dplyr
 #'
@@ -11,11 +12,11 @@
 #'
 #' @examples
 #' \dontrun{
-#' Inpatient_Demographic <- read_datalake("Inpatient_Demographic.csv", "Inpatient")}
+#' Inpatient_Demographic <- read_datalake(dir = 'dbfs:/mnt/jigsawdatalake/MSK Project' ,file= "Inpatient_Demographic.csv", folder = "Inpatient")}
 #'
-read_datalake <- function(filename, folder){
+read_datalake <- function(dir, filename, folder){
   sc <- sparklyr::spark_connect(method = 'databricks')
   opts <- list(multiline = TRUE)
-  sparklyr::spark_read_csv(sc, path = file.path('dbfs:/mnt/jigsawdatalake/MSK Project', folder, filename), opt = opts) %>%
+  sparklyr::spark_read_csv(sc, path = file.path(dir, folder, filename), opt = opts) %>%
     dplyr::collect
 }
